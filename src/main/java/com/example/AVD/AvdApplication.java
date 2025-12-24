cf152888bc86dbf3bc9357e07bd87c9eb12ccf84
Vishnu Singh <vishnusingh1057@gmail.com>
2025-12-25 01:09:56 +0530

    Init

---
+++ src/main/java/com/example/AVD/AvdApplication.java
@@ -0,0 +1,13 @@
+package com.example.AVD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AvdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/AVD/AvdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.AVD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AvdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
